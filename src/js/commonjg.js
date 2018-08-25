b490a880c83df0392599ac19e34a132ba8c3297a
gweb <gan424834453@163.com>
2018-08-25 18:43:51 +0800

    数据库

--- src/js/commonjg.js
+++ src/js/commonjg.js
@@ -2,11 +2,14 @@ jQuery(function($){
 
     // head部分
     $('#head_top').load('headandfot.html .head_top');
+    // $('.head_top').load('headandfot.html .head_top');
     // 搜索部分
     $('#pagess').load('headandfot.html .pagess');
     // 导航栏部分
     $('#nav-category').load('headandfot.html .nav-category');
+    // 放大镜
+    $('#myzooz').load('myzooz.html .zoozcj');
     // 底部
     $('#listfot').load('headandfot.html .footbox');
-
+    
 })
